58ea7bd785b6cbfa5c0f391fee50bc94eb8cbc18
ABHAY <abhay11082005@gmail.com>
2026-03-25 09:51:29 +0530

    added superuser script

--- build.sh
+++ build.sh
@@ -11,3 +11,18 @@ python manage.py collectstatic --no-input
 
 # Apply migrations
 python manage.py migrate
+
+# Create superuser if it doesn't exist
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+import os
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+if password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print('Superuser created.')
+else:
+    print('Superuser already exists or password not set.')
+"
